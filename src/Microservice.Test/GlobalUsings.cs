716c07cb62a90f9ab7f94d7016f920683470b2c5
Erfan-Edalati <erfannstreet@gmail.com>
2024-11-16 11:59:36 +0330

    Update

---
+++ src/Microservice.Test/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using FluentAssertions;
+global using Microservice.Application.Common.Interfaces;
+global using Microservice.Application.Features.UserFeatures.CommandHandlers;
+global using Microservice.Application.Features.UserFeatures.Commands;
+global using Microservice.Domain.Entities.Accounting;
+global using Moq;
---
+++ src/Microservice.Test/RoleTests/AddRoleHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿namespace Microservice.Test.RoleTests;
+
+public class AddRoleHandlerTest
+{
+    private readonly AddRoleHandler _handler;
+
+    private readonly Mock<IRepository<Role, Guid>> _repository;
+
+    private readonly Mock<IUnitOfWork> _unitOfWork;
+
+
+    public AddRoleHandlerTest()
+    {
+        _repository = new Mock<IRepository<Role, Guid>>();
+
+        _unitOfWork = new Mock<IUnitOfWork>();
+
+        _handler = new AddRoleHandler(_repository.Object,
+            _unitOfWork.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnResult()
+    {
+        _repository.Setup(r => r.AddAsync(It.IsAny<Role>(),
+            CancellationToken.None));
+
+        _unitOfWork.Setup(r => r.SaveChangesAsync(CancellationToken
+            .None))
+            .ReturnsAsync(1);
+
+        var role = new AddRoleRequest()
+        {
+            Name = "Test",
+        };
+
+        var response =
+            await _handler.Handle(role, CancellationToken.None);
+
+        response.IsSuccess.Should().BeTrue();
+    }
+}
